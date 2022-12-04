dd4d1b1732173025070e64cd66e857f259892c75
Jason Tsai <jason@pews.dev>
2022-12-04 22:51:22 +0800

    first inti

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script>
+import { onMounted } from 'vue';
+import { mountChart } from './composable/chart.js';
+
+export default {
+  name: 'Chart Demo',
+  setup() {
+    onMounted(() => {
+      const myChart = mountChart(document.getElementById('main'));
+
+      console.log(myChart);
+
+      window.onresize = () => {
+        myChart.resize();
+      };
+    });
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div id="main" class="chart"></div>
+  </div>
+</template>
+
+<style>
+body {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#app {
+  height: 100%;
+  width: 100%;
+  padding: 20px;
+}
+.container {
+  height: 600px;
+  width: 600px;
+  border-radius: 5px;
+}
+.chart {
+  width: 100%;
+  height: 100%;
+}
+</style>
